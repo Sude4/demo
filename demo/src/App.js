3ab3adf6a441c675ba89cf68b6431f87a13e2e03
Meryem Sude ÇETİN <56775168+Sude4@users.noreply.github.com>
2023-05-14 22:37:49 +0300

    .

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import  { useState } from 'react';
-
+import Axios from 'axios'
 function App() {
   const [number,setNumber]=useState()
   const [ans,setAns]=useState()
   function sendNumber(){
-fetch('http://localhost:3002/isEven',{
-  method:'POST',
-  body:JSON.stringify({val:parseInt(number)}),
-  headers:{
-    'Content-type':'application.json'
-  }
-}).then((resp)=>{return resp.json()}) .then((data)=>{setAns(data.answer)})
+Axios.post('http://localhost:3002/getUsers',{
+
+}).then((resp)=>{console.log(resp.data[0].firstName)}).catch((err)=>{console.log(err)})
   }
   return (
     <div className='App'>
--- server/index.js
+++ server/index.js
@@ -5,16 +5,38 @@ const cors=require('cors')
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const mysql=require('mysql')
 app.listen(3002,()=>{
 console.log( 'Server running at port 3002')
 })
+const db=mysql.createConnection({
+user:'react',
+host:'localhost',
+password:'Manisa.45',
+database:'project1'
+})
 
-app.post('/isEven',(req,res)=>{
-const numb=req.body.val
-if(numb%2 ==0){
-   res.send({answer:'Even'})
-}else{
- 
-    res.send({answer:'Odd'})
-}
-});
+app.post('/addUser',(req,res)=>{
+const firstName=req.body.firstName;
+const lastName=req.body.lastName;
+
+db.query("INSERT INTO users(firstName,lastName) VALUES (?,?)",[firstName,lastName],(err,res)=>{
+    if(err){
+        console.log(err)
+    }
+}) 
+})
+
+app.post('/getUsers',(req,res)=>{
+
+    db.query('SELECT * FROM users',(err,result)=>{
+        if(err){
+            console.log(err)
+        }
+
+        if(result){
+            console.log(result)
+            res.send(result)
+        }
+    })
+})
